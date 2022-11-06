de09600a301e99b889e7632b4f2d188749d03256
Steven <iftjeeper@gmail.com>
2022-11-05 23:44:31 -0400

    Scraps results from web

--- Draft.py
+++ Draft.py
@@ -1,12 +1,22 @@
 import requests
+import re
 from bs4 import BeautifulSoup as BS
 
-testWord="hard"
+testWord="easy"
 
-r = requests.get("https://www.thesaurus.com/browse"+ str(testWord).lower())
+r = requests.get("https://www.thesaurus.com/browse/"+ str(testWord).lower())
 
 want=BS(r.content, 'html.parser')
-if want.find(id="meanings"):
-    print(want.find(id="meanings"))
+tags=want.find_all(class_="css-1kg1yv8 eh475bn0")
+
+
+synonyms = []
+for things in tags:
+    synonyms.append(re.sub("/browse/","",things['href']))
+
+print(synonyms)
+
+
+
 # print(r.content)
 # print(want)
