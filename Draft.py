26b3a25e30741ffeaf8a73e478aa38d621bc7e04
Steven <iftjeeper@gmail.com>
2022-11-06 00:03:22 -0400

    Package scaper functionality into a function \"getSynonms\"

--- Draft.py
+++ Draft.py
@@ -2,21 +2,27 @@ import requests
 import re
 from bs4 import BeautifulSoup as BS
 
-testWord="easy"
+def getsynonyms(testWord):
+    '''
+    This Function take the string argument and then returns the synonyms of the
+    string from www.thesaurus.com
 
-r = requests.get("https://www.thesaurus.com/browse/"+ str(testWord).lower())
+    The returned value is a list of the synonyms
+    '''
+    r = requests.get("https://www.thesaurus.com/browse/"+ str(testWord).lower())
 
-want=BS(r.content, 'html.parser')
-tags=want.find_all(class_="css-1kg1yv8 eh475bn0")
+    want=BS(r.content, 'html.parser')
+    tags=want.find_all(class_="css-1kg1yv8 eh475bn0")
+
+    synonyms = []
+    for things in tags:
+        synonyms.append(re.sub("/browse/","",things['href']).strip())
+
+    print(synonyms)
+    return synonyms
 
 
-synonyms = []
-for things in tags:
-    synonyms.append(re.sub("/browse/","",things['href']))
 
-print(synonyms)
-
-
-
-# print(r.content)
-# print(want)
+testWord=['easy','medium','hard']
+for things in testWord:
+    getsynonyms(things)
