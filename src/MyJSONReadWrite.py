e59e98dd317a0904f1557cd67bfd721ad4275754
Steven <iftjeeper@gmail.com>
2022-11-11 15:12:59 -0500

    "Moving branch"

--- src/MyJSONReadWrite.py
+++ src/MyJSONReadWrite.py
@@ -1,26 +1,8 @@
 import json
 
-'''example output of list'''
-# # test=[
-#     'auto',
-#     'automobile',
-#     'bus',
-#     'convertible',
-#     'jeep',
-#     'limousine',
-#     'machine',
-#     'motor',
-#     'pickup',
-#     'ride',
-#     'station%20wagon',
-#     'truck',
-#     'van',
-#     'wagon']
-
-
 def writeToJSONFile(path,filename,data):
     filePathNameExt=path+filename 
-    with open(filePathNameExt,'w') as fp:
+    with open(filePathNameExt,'a') as fp:
         json.dump(data,fp)
 
 def readFromJSONFile(path,filename):
@@ -28,14 +10,5 @@ def readFromJSONFile(path,filename):
     with open(filePathNameExt,'r') as fp:
         return json.load(fp)
 
-# path='./'
-# filename='exampleWorking.json'
 
-# data={}
-# data['word']='jeep'
-# data['synonms']=test
-# # print(data)
-
-# writeToJSONFile(path,filename,data)
-# print(readFromJSONFile(path,filename))
     
